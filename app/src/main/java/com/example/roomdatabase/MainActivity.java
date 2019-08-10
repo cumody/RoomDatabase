eb5ab8aa8bc693de20046daa7940632e9b7700df
MahmoudShaaban <cumody82@gmail.com>
2019-08-10 06:50:05 -0700

    "This my RoomDatabase"

---
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.DocumentsContract;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
